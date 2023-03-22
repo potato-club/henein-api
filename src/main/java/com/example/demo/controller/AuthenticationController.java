160856c5c29c59ab2e7cc196d0454a53045a37a1
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-03-22 16:38:10 +0900

    ignore설정

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -37,7 +37,7 @@ public class AuthenticationController {
     private final JwtTokenProvider jwtTokenProvider;
 
 
-    @GetMapping("/login/kakao")
+    @PostMapping("/login/kakao")
     public ResponseEntity<?> kakaoLogin(@RequestParam String code, HttpServletResponse response) throws IOException {
         log.info("Controller -> login/kakao 진입시도 코드: "+code);
 
