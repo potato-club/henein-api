9c75467f2e65b39c7614a775412a75e62b7e8de0
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-04-02 21:46:52 +0900

    04.02 유저 이름 기본설정 guest로 설정(unique값) / 닉네임 설정하지 않고 새로고침시 에러처리임

--- src/main/java/com/example/demo/service/KakaoOAuth2UserDetailsServcie.java
+++ src/main/java/com/example/demo/service/KakaoOAuth2UserDetailsServcie.java
@@ -33,9 +33,18 @@ public class KakaoOAuth2UserDetailsServcie implements UserDetailsService {
         // 받은 정보로 찾고, 정보가 없으면 회원가입으로 갑니다.
         log.info("DB저장 service 진입");
 
+/*
+    orElse를 사용한 코드
+    PfmEntity pfm = pfmRepository.findByUserId(userId)
+                .orElse(pfmRepository.save(new PfmEntity(userId));
 
+    // orElse의 실제 동작
+    PfmEntity newPfm = pfmRepository.save(new PfmEntity(userId));
+    PfmEntity pfm = pfmRepository.findByUserId(userId)
+    .orElse(newPfm);
+ */
         UserEntity userEntity = userRepository.findByEmail(email)
-                .orElse(controlUser(email));
+                .orElseGet(() ->controlUser(email));
 
 //new UserEntity(email, guestCount.getGuestCount())
         userEntity.setRefreshToken(RT);
