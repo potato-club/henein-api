7bce94fd628eda016e15b30709e2d49cd0e227e2
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-04-03 23:56:14 +0900

    04.03 jwt 반환시 헤더 오류 수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080","http://localhost:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(true);
 
     }
 }
