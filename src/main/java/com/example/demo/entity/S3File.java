eb45367b87e5be91fd9e809b61e5664c1a76e0e9
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-04-10 15:22:25 +0900

    s3 test

---
+++ src/main/java/com/example/demo/entity/S3File.java
@@ -0,0 +1,28 @@
+//package com.example.demo.entity;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.time.LocalDateTime;
+//
+//@Entity
+//@Getter
+//@NoArgsConstructor
+//public class S3File {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "file_name")
+//    private String fileName;
+//
+//    @Column(name = "content_type")
+//    private String contentType;
+//
+//    @Column(name = "file_size")
+//    private Long fileSize;
+//
+//    @Column(name = "creation_time")
+//    private LocalDateTime creationTime;
+//}
---
+++ src/main/java/com/example/demo/repository/S3FileRespository.java
@@ -0,0 +1,7 @@
+//package com.example.demo.repository;
+//
+//import com.example.demo.entity.S3File;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface S3FileRespository extends JpaRepository<S3File,Long> {
+//}
