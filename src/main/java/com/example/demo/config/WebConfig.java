c85d126db7a9270f23c9215e29de9f79446a734e
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-05-06 20:20:57 +0900

    CORS 에러 수정을 위한 WebConfig 대폭 수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,21 +1,26 @@
 package com.example.demo.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
-@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:8080","http://localhost:3000")
-                .exposedHeaders("Authorization","RefreshToken","exception")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(false);
+public class WebConfig {
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "RefreshToken", "exception"));
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "RefreshToken", "exception"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("https://henesysback.shop/**", configuration);
+        return source;
     }
 }
---
+++ src/main/java/com/example/demo/config/WebConfig111.java
@@ -0,0 +1,21 @@
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(final CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("http://localhost:8080","http://localhost:3000")
+//                .exposedHeaders("Authorization","RefreshToken","exception")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(false);
+//
+//    }
+//}
