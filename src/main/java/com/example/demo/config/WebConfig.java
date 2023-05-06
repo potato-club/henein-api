8a1ea11e845852de61a31a09b7c97c0aab1fb3a0
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-05-06 20:11:47 +0900

    CORS 에러 수정을 위한 Security&Config 수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,15 +1,17 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@CrossOrigin(origins = {"http://localhost:8080", "http://localhost:3000"})
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080","http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:8080","http://localhost:3000")
                 .exposedHeaders("Authorization","RefreshToken","exception")
                 .allowedMethods("*")
                 .allowedHeaders("*")
