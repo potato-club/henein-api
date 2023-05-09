85ced64fe854f75f1fbfd959f167b0e602d2473d
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-05-09 23:23:35 +0900

    userInfo controller의 CORS error fix try2

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -10,8 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "RefreshToken", "exception")
-                .exposedHeaders("Authorization", "RefreshToken", "exception");
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "RefreshToken", "exception")
+                .allowCredentials(true);
     }
 }
