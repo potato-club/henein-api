a7709a74afa67f40b20d4497b3bf6f2070bf22d0
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-06-05 17:32:56 +0900

    bug fix

--- src/main/java/com/example/demo/dto/comment/ReplyResponseDto.java
+++ src/main/java/com/example/demo/dto/comment/ReplyResponseDto.java
@@ -1,22 +1,30 @@
 package com.example.demo.dto.comment;
 
 import com.example.demo.entity.ReplyEntity;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class ReplyResponseDto {
 
     private Long replyId;
+    private String userName;
     private String tag;
     private String comment;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+    private LocalDateTime modifiedDate;
     private Boolean updated;
     public ReplyResponseDto(ReplyEntity reply){
         replyId = reply.getId();
+        userName =reply.getUserName();
         tag = reply.getTag();
         comment = reply.getComment();
+        modifiedDate = reply.getCreatedDate();
         updated = reply.getUpdated();
     }
 }
