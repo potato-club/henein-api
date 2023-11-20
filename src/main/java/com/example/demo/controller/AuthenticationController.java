8f3202a9cdd912c02b825e166e59782fa5ea0244
DESKTOP-J0ABEDB\이명재 <love12mj@naver.com>
2023-11-20 21:06:30 +0900

    revise mail service

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.config.AwsSESSender;
 import com.example.demo.dto.login.BasicLoginRequestDto;
-import com.example.demo.service.EmailService;
 import com.example.demo.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,7 +25,7 @@ import java.io.UnsupportedEncodingException;
 public class AuthenticationController {
 
     private final UserService userService;
-    private final EmailService emailService;
+    private final AwsSESSender awsSESSender;
 
     //=======Local 로그인 관련
     @Operation(summary = "로컬 로그인 userEmail,password")
@@ -38,22 +38,16 @@ public class AuthenticationController {
     public ResponseEntity<String> basicSignUp(@RequestBody BasicLoginRequestDto basicLoginRequestDto, HttpServletRequest request) {
         return userService.basicSignUp(basicLoginRequestDto,request);
     }
-    @Operation(summary = "Naver 인증 메일 발송 요청")
+    @Operation(summary = "인증 메일 발송 요청")
     @PostMapping("/mail/naver")
-    public ResponseEntity<String> sendNaver(String requestEmail) throws MessagingException, UnsupportedEncodingException {
-        emailService.sendNaverMail(requestEmail);
-        return ResponseEntity.ok("인증 메일이 발송되었습니다.");
-    }
-    @Operation(summary = "Gmail 인증 메일 발송 API")
-    @PostMapping("/mail/gmail")
-    public ResponseEntity<String> sendGmail(String requestEmail) throws MessagingException, UnsupportedEncodingException {
-        emailService.sendGmail(requestEmail);
+    public ResponseEntity<String> sendNaver(String requestEmail) {
+        awsSESSender.sendToVerifyEmail(requestEmail);
         return ResponseEntity.ok("인증 메일이 발송되었습니다.");
     }
     @Operation(summary = "인증 코드 확인 API")
     @PostMapping("/verify")
     public ResponseEntity<String> verifyEmail(String key, HttpServletResponse response) {
-        emailService.verifyEmailAuth(key, response);
+        awsSESSender.verifyEmailAuth(key, response);
         return ResponseEntity.ok("이메일 인증이 정상적으로 처리되었습니다.");
     }
 
